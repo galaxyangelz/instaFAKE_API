4c8677be63664564db60634e0d20c3fe06b4ae36
galaxyangelz <nguyenhoangminhduc@gmail.com>
2016-10-04 00:26:46 +0300

    v1.2
    Final

--- routes/Posts.js
+++ routes/Posts.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var PostDatabase = require('../src/PostsData');
+var PostDatabase = require('../src/PostDatabase');
 
 function getAllPosts(req, res){
 	res.json(PostDatabase.getAllPosts());
