2e9cc39d9aca00ad2f8eb22d60e19c870888b943
galaxyangelz <nguyenhoangminhduc@gmail.com>
2016-10-04 11:21:26 +0300

    v1.5

--- src/PostDatabase.js
+++ src/PostDatabase.js
@@ -2,59 +2,23 @@ var posts = [
     {
         id: 0,
         user: {
-            id: 1,
-            username: "dtrump",
-            profileImageSmall: "http://core0.staticworld.net/images/article/2015/11/111915blog-donald-trump-100629006-primary.idge.jpg"
+            id: 2,
+            username: "ben",
+            profileImageSmall: "http://placehold.it/150x150"
         },
-        image: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
-        imageThumbnail: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
-        likes: 892,
-        caption: "Always winning #elections",
-        tags: ['elections'],
+        image: "http://placehold.it/350x350",
+        imageThumbnail: "http://placehold.it/350x350",
+        likes: 1,
+        caption: "Get Placeholder from API",
+        tags: [],
         comments: [
             {
                 id: 0,
                 user: {
                     id: 2,
-                    username: "POTUS"
+                    username: "Success"
                 },
-                comment: "You're never going to make it don #losing",
-                userRefs: [],
-                tags: ["losing"]
-            },
-            {
-                id: 1,
-                user: {
-                    id: 3,
-                    username: "HillaryC"
-                },
-                comment: "Damn right @POTUS",
-                userRefs: ["POTUS"],
-                tags: []
-            },
-        ]
-
-    },
-    {
-        id: 1,
-        user: {
-            id: 1,
-            username: "dtrump",
-            profileImageSmall: "http://core0.staticworld.net/images/article/2015/11/111915blog-donald-trump-100629006-primary.idge.jpg"
-        },
-        image: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
-        imageThumbnail: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
-        likes: 1,
-        caption: "Always lost #elections",
-        tags: ['elections'],
-        comments: [
-            {
-                id: 0,
-                user: {
-                    id: 3,
-                    username: "HillaryC"
-                },
-                comment: "Right",
+                comment: "Get successful",
                 userRefs: [],
                 tags: []
             }
