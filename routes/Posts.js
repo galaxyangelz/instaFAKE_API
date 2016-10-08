e2e6e7b893c16b7f1213c563bc56cabce19e5b89
galaxyangelz <nguyenhoangminhduc@gmail.com>
2016-10-09 02:15:14 +0300

    testing upload image

--- routes/Posts.js
+++ routes/Posts.js
@@ -1,10 +1,27 @@
+var cloudinary = require('cloudinary');
+var cloudinaryStorage = require('multer-storage-cloudinary');
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+
 var PostDatabase = require('../src/PostDatabase');
+
+// Config cloudinary storage for multer-storage-cloudinary
+var storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: 'instaFAKE', // give cloudinary folder where you want to store images
+  allowedFormats: ['jpg', 'png'],
+});
+var parser = multer({ storage: storage });
 
 function getAllPosts(req, res){
 	res.json(PostDatabase.all());
 }
+
+router.post('/upload', parser.single('image'), function (req, res) {       
+    console.log(req.file);
+    res.sendStatus(201);
+});
 
 router.route('/posts').get(getAllPosts);
 
