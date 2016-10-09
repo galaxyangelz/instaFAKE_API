f0af08e36a6deb58b791164b41e333d58c7bb93d
galaxyangelz <nguyenhoangminhduc@gmail.com>
2016-10-09 17:05:38 +0300

    Download image from cloudinary

--- routes/Posts.js
+++ routes/Posts.js
@@ -3,6 +3,7 @@ var cloudinaryStorage = require('multer-storage-cloudinary');
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
+var request = require('request');
 
 var PostDatabase = require('../src/PostDatabase');
 
@@ -14,15 +15,41 @@ var storage = cloudinaryStorage({
 });
 var parser = multer({ storage: storage });
 
+function getPostCloudinary(req, res) {
+	var url = "https://456517264879296:5PO8mpX_F5_KGfrHhVR_uTDfkMI@api.cloudinary.com/v1_1/galaxyangelzz/resources/image";
+	request(url, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+			var imageResponse = JSON.parse(body);
+			for (var i = 0; i < imageResponse.resources.length; i++) {
+				PostDatabase.add({image : imageResponse.resources[i].url});
+			}
+			console.log(PostDatabase.all());
+		    res.sendStatus(200);
+     	}
+	})
+
+	/*https.get(url, function(res) {
+		var body = '';
+		res.on('data', function(chunk) {
+			body += chunk;
+		});
+		res.on('end', function() {
+			var imageResponse = JSON.parse(body);
+			for (var i = 0; i < imageResponse.resources.length; i++) {
+				PostDatabase.add({image : imageResponse.resources[i].url});
+			}
+		});
+	}).end();*/
+}
+
 function getAllPosts(req, res){
 	res.json(PostDatabase.all());
 }
-
+router.route('/download').get(getPostCloudinary);
 router.post('/upload', parser.single('image'), function (req, res) {       
-    console.log(req.file);
     res.sendStatus(201);
 });
-
 router.route('/posts').get(getAllPosts);
 
 module.exports = router;
+//
