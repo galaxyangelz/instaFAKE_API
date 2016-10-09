1a05f9e731e8aa76367762b988600382925152c2
galaxyangelz <nguyenhoangminhduc@gmail.com>
2016-10-09 17:14:16 +0300

    change download

--- routes/Posts.js
+++ routes/Posts.js
@@ -16,14 +16,14 @@ var storage = cloudinaryStorage({
 var parser = multer({ storage: storage });
 
 function getPostCloudinary(req, res) {
+	var allPost = PostDatabase.all();
 	var url = "https://456517264879296:5PO8mpX_F5_KGfrHhVR_uTDfkMI@api.cloudinary.com/v1_1/galaxyangelzz/resources/image";
 	request(url, function (error, response, body) {
 	    if (!error && response.statusCode == 200) {
 			var imageResponse = JSON.parse(body);
 			for (var i = 0; i < imageResponse.resources.length; i++) {
-				PostDatabase.add({image : imageResponse.resources[i].url});
+				allPost[i] = {image : imageResponse.resources[i].url};
 			}
-			console.log(PostDatabase.all());
 		    res.sendStatus(200);
      	}
 	})
